817baae691d383dae565690ce0a4b8a993cc1270
adamblumoff <ablumoff25@amherst.edu>
2025-12-18 13:24:16 -0600

    fix: use legacy file system download

--- app/(tabs)/care-profile.tsx
+++ app/(tabs)/care-profile.tsx
@@ -1,6 +1,6 @@
 import { Stack } from 'expo-router';
 import * as DocumentPicker from 'expo-document-picker';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import * as Sharing from 'expo-sharing';
 import React, { useEffect, useState } from 'react';
 import {
